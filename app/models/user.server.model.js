0a66a044faa2bac86d08563abac2fb367572fe59
unknown <tyrric1066@students.ecpi.edu>
2024-09-10 22:53:50 -0400

    Initial commit

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,12 +1,77 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose'); 
 
-const UserSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    username: String,
-    password: String
-});
+const Schema = mongoose.Schema; 
 
-mongoose.model('User', UserSchema);
+  
+
+const UserSchema = new Schema({ 
+
+  firstName: { 
+
+    type: String, 
+
+    trim: true, // removes leading and trailing spaces 
+
+    required: true // required field 
+
+  }, 
+
+  lastName: { 
+
+    type: String, 
+
+    trim: true, // removes leading and trailing spaces 
+
+    required: true // required field 
+
+  }, 
+
+  email: { 
+
+    type: String, 
+
+    lowercase: true, // convert email to lowercase 
+
+    required: true, // required field 
+
+    match: [/.+\@.+\..+/, 'Please fill a valid email address'] // email validation 
+
+  }, 
+
+  username: { 
+
+    type: String, 
+
+    required: true, // required field 
+
+    trim: true, // removes leading and trailing spaces 
+
+    default: 'anonymous' // default value 
+
+  }, 
+
+  password: { 
+
+    type: String, 
+
+    required: true, // required field 
+
+    minlength: 6 // minimum password length 
+
+  }, 
+
+  created: { 
+
+    type: Date, 
+
+    default: Date.now // sets current date as the default value 
+
+  } 
+
+}); 
+
+  
+
+mongoose.model('User', UserSchema); 
+
+ 
